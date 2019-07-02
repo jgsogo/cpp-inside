2f75c7abc014cddc48410b342b931babfe103004
Javier G. Sogo <jgsogo@gmail.com>
2019-07-02 08:31:36 +0200

    is prime

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -36,7 +36,8 @@ int main(int argc, char* argv[]) {
     try
     {
         ctre::cpp::ctre CTRE(path_to_ctre_wrapper);
-        for (auto it: {"2019/02/28", "2019/02/aa", "aaa"}) {
+        for (auto it: {"1", "3", "5", "7", "10"}) {
+        //for (auto it: {"2019/02/28", "2019/02/aa", "aaa"}) {
             auto m = CTRE.match(1, it);
             std::cout << it << ": " << m << std::endl;
         }
--- library/ctre_wrapper/src/api_cpp.cpp
+++ library/ctre_wrapper/src/api_cpp.cpp
@@ -4,6 +4,7 @@
 #include "spdlog/spdlog.h"
 
 static constexpr auto pattern = ctll::fixed_string{ "(\\d{4})/(\\d{1,2})/(\\d{1,2})" };
+static constexpr auto re_is_prime = ctll::fixed_string{ ".?|(..+?)\1+"};
 
 std::unique_ptr<ctre::Status> match(const ctre::Query& query, ctre::Match& match) {
     SPDLOG_DEBUG("C++::match");
@@ -11,10 +12,19 @@ std::unique_ptr<ctre::Status> match(const ctre::Query& query, ctre::Match& match
     std::unique_ptr<ctre::Status> status = std::make_unique<ctre::Status>();
     try
     {
+        /*
         auto m = ctre::match<pattern>(query.text());
         match.set_match(m);
         match.set_regex_id(query.regex_id());
         match.set_regex(std::string(pattern.begin(), pattern.end()));
+        */
+        int number = std::stoi( query.text() );
+        auto str = std::string(number, '1');
+        SPDLOG_DEBUG(" - {}: {}", number, str);
+        auto m = ctre::match<re_is_prime>(str);
+        match.set_match(m);
+        match.set_regex_id(query.regex_id());
+        match.set_regex(std::string(re_is_prime.begin(), re_is_prime.end()));
 
         status->set_ok(true);
     }
