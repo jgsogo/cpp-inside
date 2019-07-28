4665af3b1c6f7491e5111b0c47d7fded4c7a6a12
Javier G. Sogo <jgsogo@gmail.com>
2019-07-28 19:25:30 +0200

    java project

--- examples/java/generate_java.cmake
+++ examples/java/generate_java.cmake
@@ -1,9 +1,9 @@
 # Generate Java project based on C++ CMake
 
 message("Generate Java pom.xml at ${CMAKE_CURRENT_BINARY_DIR}/pom.xml")
-message(">> ${SOURCE_DIR}")
-message(">> ${SOURCE_BINDINGS_DIR}")
-message(">> ${CRND_LIBRARY_PATH}")
+message(">> SOURCE_DIR: ${SOURCE_DIR}")
+message(">> SOURCE_BINDINGS_DIR: ${SOURCE_BINDINGS_DIR}")
+message(">> CRND_LIBRARY_PATH: ${CRND_LIBRARY_PATH}")
 
 configure_file(${SOURCE_DIR}/pom.xml.template ${CMAKE_CURRENT_BINARY_DIR}/pom.xml @ONLY)
 configure_file(${SOURCE_DIR}/src/main/java/crnd/App.java.template ${CMAKE_CURRENT_BINARY_DIR}/src/main/java/crnd/App.java @ONLY)
