8b94415af554494e985f743284e97d36bdd8c4bc
Javier G. Sogo <jgsogo@gmail.com>
2019-07-28 13:02:06 +0200

    building the python binding

--- bindings/python/crnd.py
+++ bindings/python/crnd.py
@@ -1,25 +1,61 @@
 
 import ctypes
+from messages import status_pb2, help_pb2, sample_request_pb2, sample_pb2, model_pb2
 
 
 class ProtoSerialized(ctypes.Structure):
+    _fields_ = [('data', ctypes.c_char_p),
+                ('size', ctypes.c_int),]
 
-    _fields_ = [('data', ctypes.c_void_p),
-                ('size', ctypes.POINTER(ctypes.c_int)),]
+
+def get_callback(data, status):
+    @ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.POINTER(ProtoSerialized), ctypes.POINTER(ProtoSerialized))
+    def callback(_, data_in, status_in):
+        status = status_pb2.Status()
+        status.ParseFromString(status_in.contents.data)
+        if status.ok:
+            help = help_pb2.Help()
+            help.ParseFromString(data_in.contents.data)
+        else:
+            print("ERROR")
+
+    return callback
 
 
 class CRND:
     def __init__(self, path_to_library):
-        # self.path_to_library = path_to_library
         self.dll = ctypes.cdll.LoadLibrary(path_to_library)
 
+    def _call(self, f, data, *input_args):
+        status = status_pb2.Status()
+
+        @ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.POINTER(ProtoSerialized), ctypes.POINTER(ProtoSerialized))
+        def callback(_, data_in, status_in):
+            status.ParseFromString(status_in.contents.data)
+            if status.ok:
+                data.ParseFromString(data_in.contents.data)
+
+        args = list(input_args) + [callback,]
+        f(None, *args)
+
+        if status.ok:
+            return data, status
+        else:
+            return None, status
+
+    def _sample(self, seed, samples, model):
+        sample_request = sample_request_pb2.SampleRequest()
+        sample_request.seed = seed
+        sample_request.n_samples = samples
+        sample_request.model = model
+
+        sample = sample_pb2.Sample()
+        sample, status = self._call(self.dll.sample, sample, sample_request)
+
     def help(self, output):
-        @ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p)
-        def callback(data, status):
-
-            print("foo has finished its job")
-
-        self.dll.help(None, callback)
+        help = help_pb2.Help()
+        help, status = self._call(self.dll.help, help)
+        print(help)
 
     def uniform(self, seed, samples):
         pass
@@ -28,7 +64,11 @@ class CRND:
         pass
 
     def lognormal(self, seed, samples, mean, stddev):
-        pass
+        model = model_pb2.Model()
+        model.id = model_pb2.Model.LOGNORMAL
+        model.params["mean"] = mean
+        model.params["stddev"] = stddev
+        return self._sample(seed, samples, model)
 
 
 if __name__ == '__main__':
@@ -37,3 +77,5 @@ if __name__ == '__main__':
     crnd = CRND(path_to_lib)
     print(crnd.dll)
     crnd.help(None)
+
+    crnd.lognormal(seed=1234, samples=10, mean=2, stddev=0.2)
