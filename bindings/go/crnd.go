c6ef13f755f3ed5af0eff29810b55e9253d58b92
Javier G. Sogo <jgsogo@gmail.com>
2019-08-03 20:34:46 +0200

    example with protobuf

--- bindings/go/crnd.go
+++ bindings/go/crnd.go
@@ -1,5 +1,24 @@
 package crnd
 
+import (
+    "fmt"
+    "log"
+
+    messages "./messages"
+    proto "github.com/golang/protobuf/proto"
+)
+
 func Hello() string {
-    return "Hello, world, from library!"
+    p := &messages.Help{
+        Name:    "name",
+        }
+
+    data, err := proto.Marshal(p)
+    if err != nil {
+      log.Fatal("marshaling error: ", err)
+    }
+
+    //fmt.Println(data)
+    return proto.MarshalTextString(p)
+    //return "Hello, world, from library!"
 }
