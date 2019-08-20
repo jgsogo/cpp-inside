2ec2b4323ad61b5408dc64b999b6ab3f0be5c464
Javier G. Sogo <jgsogo@gmail.com>
2019-08-20 23:21:50 +0200

    trying dyn load

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -4,31 +4,47 @@
 #![allow(non_snake_case)]
 
 include!("bindings.rs");
+extern crate libloading;
 
-//use protobuf::*;
-//use protos::help::Help;
+/* From bindings.rs
+pub type callback_t = ::std::option::Option< unsafe extern "C" fn (state: *mut ::std::os::raw::c_void,
+                                                                   data: *const ::std::os::raw::c_void,
+                                                                   status: *const ::std::os::raw::c_void)>;
 
-// Wrapper
-/*
-fn rust_function<F: Fn(*const ::std::os::raw::c_void, *const ::std::os::raw::c_void)>(callback: F) {
-    let callback: Box<dyn Fn(*const ::std::os::raw::c_void, *const ::std::os::raw::c_void)> = Box::new(callback);
-
-    extern "C" fn callback_wrapper(state: *mut ::std::os::raw::c_void,
-                                   data: *const ::std::os::raw::c_void,
-                                   status: *const ::std::os::raw::c_void) {
-        let f = state as *const Box<dyn Fn(*const ::std::os::raw::c_void, *const ::std::os::raw::c_void)>;
-        unsafe { f.as_ref().unwrap()(data); f.as_ref().unwrap()(status);  }
-    }
-    help(callback_wrapper, &callback as *const _ as *const ::std::os::raw::c_void);
+extern "C" {
+    pub fn help(state: *mut ::std::os::raw::c_void, help_callback: callback_t);
 }
+
 */
+
 
 pub mod protos;
 
 use protobuf::*;
 use protos::help::Help;
+use std::mem;
+use libloading::{Library, Symbol};
+
+
+extern "C" fn help_handler(state: *mut ::std::os::raw::c_void,
+                           data: *const ::std::os::raw::c_void,
+                           status: *const ::std::os::raw::c_void) {
+    let closure: &mut &mut FnMut(*const ::std::os::raw::c_void, *const ::std::os::raw::c_void) = unsafe { mem::transmute(state) };
+    closure(data as *const ::std::os::raw::c_void, status as *const ::std::os::raw::c_void)
+}
+
+pub fn help_with_callback<F>(mut callback: F)
+    where F: FnMut(*const ::std::os::raw::c_void, *const ::std::os::raw::c_void)
+{
+    // reason for double indirection is described below
+    let mut cb: &mut FnMut(*const ::std::os::raw::c_void, *const ::std::os::raw::c_void) = &mut callback;
+    let cb = &mut cb;
+    unsafe { help(cb as *mut _ as *mut ::std::os::raw::c_void, Some(help_handler)) }
+}
+
 
 pub struct crnd {
+    pub lib: Library
 }
 
 impl crnd {
@@ -36,20 +52,21 @@ impl crnd {
         "Hello!".to_string()
     }
 
-    pub fn help() -> String {
-        let mut help_message = Help::new();
-        help_message.set_name("name".to_string());
+    pub fn help(self) -> String {
+        type HelpFunc = fn(*mut ::std::os::raw::c_void, *const ::std::os::raw::c_void, *const ::std::os::raw::c_void);
+        let func: Symbol<HelpFunc> = unsafe {self.lib.get(b"help").unwrap()};
 
-        let f = || {};
+        let mut help_message = Help::new();
+
+        // Use a callback with closure to populate help_message
+        help_with_callback(|data, status| {
+            help_message.set_name("inner".to_string());
+        });
+
+        //help_message.set_name("name".to_string());
 
         let mut contents = String::new();
         help_message.write_to_writer(unsafe {contents.as_mut_vec()});
         contents
     }
 }
-
-/*
-pub type callback_t = ::std::option::Option<unsafe extern "C" fn (state: *mut ::std::os::raw::c_void,
-                                                                  data: *const ::std::os::raw::c_void,
-                                                                  status: *const ::std::os::raw::c_void)>;
-*/
--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -1,7 +1,18 @@
 extern crate crnd;
+extern crate libloading;
+
+use std::env;
+use libloading::{Library, Symbol};
 
 fn main() {
+    let library_path = env::args().nth(1).expect("USAGE: loading <LIB>");
+
+    let instance = crnd::crnd{
+        lib: Library::new(library_path).unwrap()
+    };
+    //instance.lib = Library::new(library_path).unwrap();
+
     println!("Hello World!");
     println!("from the library: {}", crnd::crnd::hello());
-    println!("from the library | help: {}", crnd::crnd::help());
+    println!("from the library | help: {}", instance.help());
 }
