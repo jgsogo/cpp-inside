cd6941b37606f9387712bc394a2c5528079198f1
Javier G. Sogo <jgsogo@gmail.com>
2019-09-08 12:04:02 +0200

    ignore files

--- emscripten/build.sh
+++ emscripten/build.sh
@@ -1,5 +1,5 @@
-mkdir build
-pushd build
+mkdir _build
+pushd _build
 conan install ../../conanfile.py --profile ../profile --build=missing -o csharp=False -o java=False -o objc=False -o php=False -o python=False -o ruby=False -o go=False -o cpp=False
 conan source ../../conanfile.py --install-folder=. --source-folder=../../
 conan build ../../conanfile.py
