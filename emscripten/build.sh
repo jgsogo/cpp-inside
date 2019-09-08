54fda6bac7a47c799ec942e2364efcce7c001e1c
Javier G. Sogo <jgsogo@gmail.com>
2019-09-08 12:11:55 +0200

    activate and run

--- emscripten/build.sh
+++ emscripten/build.sh
@@ -1,7 +1,11 @@
 mkdir _build
+
 pushd _build
-conan install ../../conanfile.py --profile ../profile --build=missing -o csharp=False -o java=False -o objc=False -o php=False -o python=False -o ruby=False -o go=False -o cpp=False
+conan install ../../conanfile.py -g virtualenv --profile ../profile --build=missing -o csharp=False -o java=False -o objc=False -o php=False -o python=False -o ruby=False -o go=False -o cpp=False
 conan source ../../conanfile.py --install-folder=. --source-folder=../../
 conan build ../../conanfile.py
 conan package ../../conanfile.py --package-folder=../
 popd
+
+source _build/activate.sh
+emrun ./bin/libcrnd.html
