3afa152444e173d71d5a137f9cd0557eb53f8a2f
Javier G. Sogo <jgsogo@gmail.com>
2019-09-14 20:37:48 +0200

    do not add duplicated

--- js/twitter.js
+++ js/twitter.js
@@ -37,17 +37,45 @@ function get_user(user_id, on_user_data) {
     });
 }
 
+function add_choosen(username) {
+    var choosen = [];
+    $('#all-choosen li').each(function(i) {
+        choosen.push($(this).text());
+    });
+    if (choosen.indexOf(username) < 0) {
+        $("#all-choosen").append($('<li>' + username + '</li>'));
+    }
+}
+
 function draft_one(username, tweet_id) {
     $("#choosen").empty();
     get_retweeters(tweet_id, function(user_ids){
-        console.log(user_ids);
+        console.log("User IDs:" + user_ids);
         var user_id = user_ids[Math.floor(Math.random() * user_ids.length)];
         get_user(user_id, function(data) {
             $("#choosen").append($('<img src="' + data.profile_image_url_https + '" class="avatar"/>'));
             $("#choosen").append($('<p>' + data.name + '</p>'));
-            $("#choosen").append($('<p>@' + data.screen_name + '</p>'));
+            $("#choosen").append($('<p><small><img src="img/twitter-logo.svg" class="brand-icon"/>@' + data.screen_name + '</small></p>'));
 
-            $("#all-choosen").append($('<li>@' + data.screen_name + '</li>'));
+            add_choosen(data.screen_name);
+        });
+    });
+}
+
+function draft_one_seed(username, tweet_id) {
+    $("#choosen-seed").empty();
+    get_retweeters(tweet_id, function(user_ids){
+        console.log("User IDs:" + user_ids);
+        
+        var seed = $("#seed").val();
+        if (seed) { Math.seedrandom(seed); } else { Math.seedrandom();}
+        var idx = Math.floor(Math.random() * user_ids.length);        
+        get_user(user_ids[idx], function(data) {
+            $("#choosen-seed").append($('<img src="' + data.profile_image_url_https + '" class="avatar"/>'));
+            $("#choosen-seed").append($('<p>' + data.name + '</p>'));
+            $("#choosen-seed").append($('<p><small><img src="img/twitter-logo.svg" class="brand-icon"/>@' + data.screen_name + '</small></p>'));
+
+            add_choosen(data.screen_name);
         });
     });
 }
